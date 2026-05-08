dfcf48346b453bcd0dc7caaf831fe36d8ef43a59
Reis <renanreislim4@gmail.com>
2026-05-07 21:10:51 -0300

    feat(07/05) ajustes de rota

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-const apiPorta = process.env.NEXT_PUBLIC_API_PORTA;
-
 const api = axios.create({
-  baseURL: `${apiUrl}:${apiPorta}`, // ip e porta do servidor
+  baseURL: "http://localhost:5000/api", 
 });
 
 export default api;
